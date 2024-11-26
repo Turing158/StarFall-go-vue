e4cac516e9bd190f35097be612c341b0a2481d4b
Turing <15818961209@163.com>
2024-11-27 02:07:41 +0800

    Enhance error handling logic by adding prompt messages for tokens without access permissions.
    增强错误处理逻辑，添加对无访问权限token的提示信息

--- src/util/request.js
+++ src/util/request.js
@@ -35,6 +35,8 @@ request.interceptors.response.use((res)=>{
     ElMessage.error("为获取到用户信息，请登录！")
   }else if(errMsg == "The token has expired" || errMsg == "Token is expired"){
     ElMessage.error("token已过期,请重新登录！")
+  }else if(errMsg == "The current token does not allow access, please log in again"){
+    ElMessage.error("当前token不允许访问，请重新登录!")
   }
   return Promise.reject(err)
 })
