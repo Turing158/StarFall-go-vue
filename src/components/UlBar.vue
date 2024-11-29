1f53aa345ffed2427b29c98f22ddb1449c161a9b
Turing <15818961209@163.com>
2024-11-29 16:50:31 +0800

    解决了子菜单昵称不居中问题
    解决了子菜单昵称不居中问题

--- src/components/UlBar.vue
+++ src/components/UlBar.vue
@@ -263,9 +263,9 @@ ul .avatar_img {
   font-size: 18px;
   width: 100%;
   height: 50px;
-  text-align: center;
   display: flex;
   align-items: center;
+  justify-content: center;
 }
 .menu .exp {
   position: relative;
